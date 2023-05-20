0ac9735fc050a3a921aa23b360b874118abd2de2
abhiamber191 <abhiamber191@gmail.com>
2023-05-20 12:00:23 +0530

    done

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 const userRoute = require("./routes/user.routes");
 const scheduleRoute = require("./routes/schedule.route");
-const eventRoute = require("./routes/event.routes");
+// const eventRoute = require("./routes/event.routes");
 
 const PORT = process.env.PORT || 8080;
 
 // middleware;
-app.use(cors());  
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/schedule", scheduleRoute);
-app.use("/api/v1/event", eventRoute);
+// app.use("/api/v1/event", eventRoute);
 
 app.get("/", async (req, res) => {
   res.send("api is working fine............");
